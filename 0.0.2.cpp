93a6eda590d72c4a818e939f21180bff96874f1a
X4Ker <32191102+X4Ker@users.noreply.github.com>
2017-11-10 17:15:17 +0400

    Create 0.0.2.cpp

---
+++ 0.0.2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
+using namespace std;
+
+bool read_numbers(int *a, unsigned int n) {
+    string string;
+    int k;
+    getline(cin, string);
+    istringstream stream(string);
+    for (int i = 0; i < n; ++i) {
+        if (!(stream >> a[i])) {
+            return false;
+            break;
+        }
+    }
+    if (stream >> n){
+        return false;
+        exit(0);
+    }
+    return true;
+}
+
+void revers(int *a, unsigned int n){
+    for (unsigned int i = 0; i < n/2; i++){
+        swap(a[i], a[n - i - 1]);
+    }
+}
+
+int main() {
+    int *a;
+    int n, k;
+    cin >> n;
+    if (n >= 0) {
+        cin.get();
+        a = new int[n];
+        if (read_numbers(a, n)) {
+            cin >> k;
+            revers(a, n - k);
+            for (int i = 0; i < n; i++) {
+                cout << a[i] << " ";
+            }
+            cout << endl;
+            revers(a + n - k, k);
+            for (int i = 0; i < n; i++) {
+                cout << a[i] << " ";
+            }
+            cout << endl;
+            revers(a, n);
+            for (int i = 0; i < n; i++) {
+                cout << a[i] << " ";
+            }
+        } else {
+            cout << "An error has occured while reading input data";
+        }
+    }
+    else {
+        cout << "An error has occured while reading input data";
+    }
+    cin.get();
+    return 0;
+}
